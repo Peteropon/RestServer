a2ddbc08dd1fb055069da0c31e647fe62be44503
Petros Tsavdaridis <peteropon@gmail.com>
2019-03-16 11:20:21 +0100

    Populated animal service impl with the necessary methods

--- src/main/java/com/petros/restserver/model/AnimalServiceImpl.java
+++ src/main/java/com/petros/restserver/model/AnimalServiceImpl.java
@@ -2,10 +2,10 @@ package com.petros.restserver.model;
 
 
 import com.petros.restserver.service.AnimalService;
-import com.petros.restserver.util.AnimalNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service("animalService")
@@ -14,6 +14,10 @@ public class AnimalServiceImpl implements AnimalService {
     public static final AtomicLong counter =  new AtomicLong();
 
     private static List<Animal> animals;
+
+    static {
+        animals = populateAnimals();
+    }
 
 
     @Override
@@ -38,16 +42,20 @@ public class AnimalServiceImpl implements AnimalService {
 
     @Override
     public void deleteAnimalById(Long id) {
-
+        animals.removeIf(animal -> animal.getId() == id);
     }
 
     @Override
     public void deleteAllAnimals() {
-
+        animals.clear();
     }
 
     @Override
     public Animal findByName(String name) {
+        for (Animal animal: animals  ) {
+            if(animal.getName().equalsIgnoreCase(name))
+            return animal;
+        }
         return null;
     }
 
@@ -55,4 +63,17 @@ public class AnimalServiceImpl implements AnimalService {
     public List<Animal> findAllAnimals() {
         return null;
     }
+
+    private static List<Animal> populateAnimals() {
+        CopyOnWriteArrayList<Animal> animals = new CopyOnWriteArrayList<>();
+        animals.add(new Animal(counter.incrementAndGet(), "Fox", "Orange", "Canidae"));
+        animals.add(new Animal(counter.incrementAndGet(), "Bear", "Brown", "Ursidae"));
+        animals.add(new Animal(counter.incrementAndGet(), "Mongoose", "Brown", "Herpestidae"));
+        animals.add(new Animal(counter.incrementAndGet(), "Meerkat", "Yellow", "Herpestidae"));
+        animals.add(new Animal(counter.incrementAndGet(), "Lion", "Yellow", "Felidae"));
+        animals.add(new Animal(counter.incrementAndGet(), "Raccoon", "Grey", "Procyonidae"));
+        animals.add(new Animal(counter.incrementAndGet(), "Panther", "Black", "Felidae"));
+        return animals;
+    }
+
 }
