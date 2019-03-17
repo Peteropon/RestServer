58f131442aa04afeda85a5ef7c30e9ea7e585414
Petros Tsavdaridis <peteropon@gmail.com>
2019-03-17 12:21:43 +0100

    Modified actuator settings, deleted unused service class

--- src/main/java/com/petros/restserver/controller/RestApiController.java
+++ src/main/java/com/petros/restserver/controller/RestApiController.java
@@ -12,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
